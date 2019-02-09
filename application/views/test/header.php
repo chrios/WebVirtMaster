4018c37e23664bc235e321224e6f68f310788071
Christopher Frew <chris@mawson.tech>
2019-02-09 17:21:34 +0930

    added test/ views and into controller

---
+++ application/views/test/header.php
@@ -0,0 +1,8 @@
+<h1>Virtual server management</h1>
+<a href="?">Main page</a>
+| <a href="?action=host-information">Host information</a>
+| <a href="?action=virtual-networks">Virtual networks</a>
+| <a href="?action=node-devices">Node devices</a>
+| <a href="?action=storage-pools">Storage pools</a>
+| <a href="?action=nwfilters">Network filters</a>
+<br/><br/>
